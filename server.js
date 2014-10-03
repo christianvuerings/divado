1c0766a3e179118fcefad39f72bb9942e2a10d1f
Christian Vuerings <vueringschristian@gmail.com>
2014-10-02 18:30:01 -0700

    Update localhost reference

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.get('/', function(req, res) {
 });
 
 var port = process.env.PORT || 5000;
-server.listen(port, 'localhost');
+// server.listen(port, 'localhost');
+server.listen(port);
 server.on('listening', function() {
   console.log('Express server started on port %s at %s', server.address().port, server.address().address);
 });
